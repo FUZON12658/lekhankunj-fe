1fe986c6a48b89ab7dad9eb5d922e30e10efb471
fuzon12658 <fuzon12658@gmail.com>
2025-08-29 11:36:07 +0545

    logo updated

--- components/home/bookTypes.jsx
+++ components/home/bookTypes.jsx
@@ -1,18 +1,20 @@
+import { BookIcon } from "lucide-react";
 import Image from "next/image";
+import { AudioBookIcon, EbookIcon } from "../common/icons";
 
 const BookTypes = ({ type }) => {
     const metadata =
         type === "physical" ?
-            { title: "Physical Book", subtitle: "Fastest Shipping", icon: "/placeholder.png", background: "bg-[#418295]" } :
+            { title: "Physical Book", subtitle: "Fastest Shipping", icon: <BookIcon className="w-10 h-10 text-white"/>, background: "bg-[#418295]" } :
             type === "ebook" ?
-                { title: "eBooks", subtitle: "Instant Access", icon: "/placeholder.png", background: "bg-[#41957F]" } :
+                { title: "eBooks", subtitle: "Instant Access", icon: <EbookIcon  className={`w-10 h-10`} />, background: "bg-[#41957F]" } :
                 type === "audiobook" ?
-                    { title: "Audiobook", subtitle: "From Best Narrators", icon: "/placeholder.png", background: "bg-[#4A4195]" } : null;
+                    { title: "Audiobook", subtitle: "From Best Narrators", icon: <AudioBookIcon className={`w-10 h-10`} />, background: "bg-[#4A4195]" } : null;
 
     return (
         <div className="bg-[#F8F8F8] rounded-2xl flex items-center gap-4 px-5 py-4 min-w-2xs">
             <div className={`w-16 h-16 grid place-items-center rounded-full ${metadata.background}`}>
-                <Image src={metadata.icon} alt="Icon" width={32} height={32} />
+                {metadata.icon }
             </div>
             <div>
                 <h3 className="text-subtitle-1 text-primary-700 pb-1">{metadata.title}</h3>
